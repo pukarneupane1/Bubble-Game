2ee84dfba1cc8f375003d9ac3bf9aa5693162561
pukar <pukarneupane04@gmail.com>
2024-05-06 22:25:12 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit() {
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+function makeBubble() {
+
+    var clutter = "";
+
+
+    for (var i = 1; i <= 102; i++) {
+        var rn = Math.floor(Math.random() * 10);
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+
+    document.querySelector("#pbottom").innerHTML = clutter;
+}
+
+function runTimer() {
+    var timerint = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timervalue").textContent = timer;
+        }
+        else {
+            clearInterval(timerint);
+            document.querySelector("#pbottom").innerHTML = `<h1>Game Over</h1>`;
+        }
+    },
+        1000); /* their 1000 means setInterval will run every seconds by giving their 1000 */
+}
+
+document.querySelector("#pbottom")
+.addEventListener("click", function(dets){
+    var clickednum = Number(dets.target.textContent);
+    if(clickednum === hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+});
+
+makeBubble();
+runTimer();
+getNewHit();
+
